9d430e591b2238ae56a4a7bc9ff42c67d8749a20
Yusuf Sonmez <yusufsn54@gmail.com>
2023-11-13 22:42:22 +0300

    Recursive bug fixed
    * get_weather_data function were calling itself repedatly. Change with manifacture_weather_data function.

--- Web/app/routes.py
+++ Web/app/routes.py
@@ -1,6 +1,8 @@
 import random
 import numpy as np
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Blueprint
+
+bp = Blueprint('main', __name__)
 
 def manifacture_weather_data():
     month = random.choice(['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'])
@@ -11,11 +13,8 @@ def manifacture_weather_data():
 
     return {'month': month, 'temp': temperature, 'RH': relative_humidity, 'wind': wind, 'rain': rain}
 
-
-app = Flask(__name__)
-
-@app.route('/api/weather', methods=['GET'])
+@bp.route('/api/weather', methods=['GET'])
 def get_weather_data():
-    weather_data = get_weather_data()
+    weather_data = manifacture_weather_data()
 
     return jsonify(weather_data)
